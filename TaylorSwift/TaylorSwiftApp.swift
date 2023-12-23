3949b452656ef8e3c78f792a9db6a1b560b29237
monica255 <monicasucianto123@gmail.com>
2023-12-23 23:31:44 +0800

    Initial Commit

---
+++ TaylorSwift/TaylorSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaylorSwiftApp.swift
+//  TaylorSwift
+//
+//  Created by Monica Sucianto on 23/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TaylorSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
